740dba788cc5a8fb5a33f963732add90527d29c1
aniellonigrocogliano <aniello.nigro.cogliano@gmail.com>
2024-07-03 17:16:19 +0200

    Add: messaggi in italiano

--- app/Http/Requests/WineRequest.php
+++ app/Http/Requests/WineRequest.php
@@ -30,4 +30,21 @@ class WineRequest extends FormRequest
         'image'=> 'nullable',
         ];
     }
+    public function messages(): array {
+        return[
+        'winery.required' => 'Il nome della vineria è obligatoria',
+        'winery.min' => 'Il nome della vineria deve contenere almeno 3 caratteri',
+        'winery.max' => 'Il nome della vineria non può contenere più di 255 caratteri',
+        'wine.required' => 'Il nome del vino è obligatoria',
+        'wine.min' => 'Il nome del vino deve contenere almeno 3 caratteri',
+        'wine.max' => 'Il nome del vino non può contenere più di 255 caratteri',
+        'average.required'=>'La valutazione è obligatoria',
+        'average.decimal'=>'La valutazione deve essere un decimale con solo un numero dopo la virgola',
+        'reviews.required' => 'Il numero di recensione è obligatorio',
+        'reviews.integer' => 'Il numero di recensioni deve essere intero',
+        'location.min' => 'Il nome della provenienza deve contenere almeno 3 caratteri',
+        'location.max' => 'Il nome della provenienza non può contenere più di 255 caratteri',
+        'location.required' => 'Il nome della provenienza è obligatoria',
+        ];
+    }
 }
